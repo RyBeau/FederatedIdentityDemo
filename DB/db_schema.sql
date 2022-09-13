2d712057c3705e71a4ee3556739a53d7351a915c
Ryan Beaumont <ryan.beaumont1@gmail.com>
2022-09-13 23:05:57 +1200

    Create testing db schema and demo users

---
+++ DB/db_schema.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE IF NOT EXISTS AUTH_DEMO;
+
+CREATE TABLE roles (
+    id int AUTO_INCREMENT,
+    role VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE users (
+    id int AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    role_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES roles(id)
+);
+
+INSERT INTO roles (role)
+VALUES ('Admin');
+INSERT INTO roles (role)
+VALUES ('Developer');
+INSERT INTO roles (role)
+VALUES ('Basic');
+
+INSERT INTO users (username, password, role_id)
+VALUES ("ryan.beaumont", SHA2('ryanspassword', 256), 1);
+
+INSERT INTO users (username, password, role_id)
+VALUES ("mr.dev", SHA2('devspassword', 256), 2);
+
+INSERT INTO users (username, password, role_id)
+VALUES ("mr.basic", SHA2('basicspassword', 256), 3);
